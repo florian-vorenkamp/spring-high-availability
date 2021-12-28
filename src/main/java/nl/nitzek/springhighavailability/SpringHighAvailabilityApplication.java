eed54f81852d527167f0ee84c640a3b112489628
Florian Vorenkamp <florian.vorenkamp@topicus.nl>
2021-12-28 10:35:40 +0100

    Initial commit

---
+++ src/main/java/nl/nitzek/springhighavailability/SpringHighAvailabilityApplication.java
@@ -0,0 +1,13 @@
+package nl.nitzek.springhighavailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHighAvailabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHighAvailabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/nitzek/springhighavailability/SpringHighAvailabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.nitzek.springhighavailability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHighAvailabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
